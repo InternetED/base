6ed206cc304ff6d81630dc24cabbed40b1fbabc4
Lin <salahayo3192@gmail.com>
2018-12-05 18:35:25 +0800

    2018/12/05(三) 1.更新 DataBinding 的 base 包裡的基礎類

--- app/src/main/java/com/internet/boy/base/MainActivity.kt
+++ app/src/main/java/com/internet/boy/base/MainActivity.kt
@@ -1,14 +1,33 @@
 package com.internet.boy.base
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.internet.boy.androidbase.base.common.BaseActivity
+import com.internet.boy.androidbase.expansinon.closeFullScreen
+import com.internet.boy.androidbase.expansinon.openFullScreen
+import com.internet.boy.androidbase.expansinon.toast
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    override val layoutId: Int
+        get() = R.layout.activity_main
 
 
+
+    override fun initView(savedInstanceState: Bundle?) {
+
+
+
+        btnBegin.setOnClickListener {
+
+            toast("$isFastClick")
+            closeFullScreen()
+        }
+
+        btnEnd.setOnClickListener {
+            openFullScreen()
+        }
     }
+
+
 }
